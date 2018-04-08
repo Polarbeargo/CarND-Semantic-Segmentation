d9929bddcc2cc422aca2b9d3db2fc97f7cf260f3
alice <bow1226@gmail.com>
2018-04-08 12:15:14 +0800

    Fixed import urllib problem in python 2
    https://stackoverflow.com/questions/31601238/python-2-7-10-error-from-urllib-request-import-urlopen-no-module-named-request

--- helper.py
+++ helper.py
@@ -8,7 +8,7 @@ import zipfile
 import time
 import tensorflow as tf
 from glob import glob
-from urllib.request import urlretrieve, Request
+from urllib import urlretrieve
 from tqdm import tqdm
 
 class DLProgress(tqdm):
