175928417435271047870d57036d7f7f8695227a
alice <bow1226@gmail.com>
2018-04-08 12:05:03 +0800

    Fixing urllib.request import problem and typo error
    Fixing urllib.request import problem and typo error

--- helper.py
+++ helper.py
@@ -8,7 +8,7 @@ import zipfile
 import time
 import tensorflow as tf
 from glob import glob
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, Request
 from tqdm import tqdm
 
 class DLProgress(tqdm):
@@ -88,7 +88,7 @@ def gen_batch_function(data_folder, image_shape):
                     scipy.misc.imread(gt_image_file), image_shape)
 
                 gt_bg = np.all(gt_image == background_color, axis=2)
-                gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
+                gt_bg = gt_bg.reshape(gt_bg.shape, 1)
                 gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
 
                 images.append(image)
