4f7e408bacbdcd1ae5611927ed22168687251e79
alice <bow1226@gmail.com>
2018-08-09 14:15:45 +0800

    Fix softmax_cross_entropy_with_logits() argument usage

--- main.py
+++ main.py
@@ -113,12 +113,12 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    logits = tf.reshape(input, (-1, num_classes))
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
     correct_label = tf.reshape(correct_label, (-1, num_classes))
 
     # define loss function
     cross_entropy_loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits, labels))
+        tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = correct_label))
 
     # define training operation
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
